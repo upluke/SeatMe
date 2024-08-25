4ce7628a55fd5ab5f7a9702092b8e79ab2d44b1f
upluke <lukeforadream@gmail.com>
2024-08-25 10:15:40 -0700

    set cors and basic ui fetch

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetchBookings();
+  }, []);
+
+  const fetchBookings = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/bookings/");
+      const data = await response.json();
+      // console.log(data);
+      setBookings(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <h1>Booking System</h1>
@@ -9,6 +27,12 @@ function App() {
         <input type="number" placeholder="Number of people" />
         <button>Add Booking</button>
       </div>
+      {bookings.map((booking) => (
+        <div key={booking.id}>
+          <p>Name: {booking.guess_name} </p>
+          <p>Number of People: {booking.head_count}</p>
+        </div>
+      ))}
     </>
   );
 }
--- server/newproject/newproject/settings.py
+++ server/newproject/newproject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'newproject.urls'
@@ -123,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ALlows all only for demo purposes
+CORS_ALLOW_ALL_ORIGINS= True
