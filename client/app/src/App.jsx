52b963e5b639620914c5c4e6b456738d845d9482
upluke <lukeforadream@gmail.com>
2024-08-25 09:29:48 -0700

    set up frontend

---
+++ client/app/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Booking System</h1>
+      <div>
+        <input type="text" placeholder="Name" />
+        <input type="number" placeholder="Number of people" />
+        <button>Add Booking</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
