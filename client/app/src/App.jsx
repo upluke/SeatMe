c0553f395bcdeef33c8bade4bf2bd6eed32b04f8
upluke <lukeforadream@gmail.com>
2024-08-25 12:46:04 -0700

    create update UI and func

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -6,6 +6,9 @@ function App() {
   const [guestName, setGusetName] = useState("");
   const [headCount, setHeadCount] = useState(0);
 
+  const [newGuestName, setNewGuestName] = useState("");
+  const [newHeadCount, setNewHeadCount] = useState(0);
+
   useEffect(() => {
     fetchBookings();
   }, []);
@@ -47,6 +50,59 @@ function App() {
     }
   };
 
+  const updateBooking = async (fieldToBeUpdated, booking) => {
+    const target_pk = booking.id;
+    let bookingData;
+    if (fieldToBeUpdated === "guestName") {
+      bookingData = {
+        guest_name: newGuestName,
+        head_count: booking.head_count,
+      };
+    } else {
+      bookingData = {
+        guest_name: booking.guest_name,
+        head_count: newHeadCount,
+      };
+    }
+
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:8000/api/bookings/${target_pk}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(bookingData),
+        }
+      );
+      const data = await response.json();
+      setBookings((prev) =>
+        prev.map((booking) => {
+          if (booking.id === target_pk) {
+            return data;
+          } else {
+            return booking;
+          }
+        })
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteBooking = async (pk) => {
+    try {
+      await fetch(`http://127.0.0.1:8000/api/bookings/${pk}`, {
+        method: "DELETE",
+      });
+
+      setBookings((prev) => prev.filter((booking) => booking.id !== pk));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <h1>Booking System</h1>
@@ -67,6 +123,23 @@ function App() {
         <div key={booking.id}>
           <p>Name: {booking.guest_name} </p>
           <p>Number of People: {booking.head_count}</p>
+          <input
+            type="text"
+            placeholder="New guest name"
+            onChange={(e) => setNewGuestName(e.target.value)}
+          />
+          <button onClick={() => updateBooking("guestName", booking)}>
+            Change Name
+          </button>
+          <input
+            type="number"
+            placeholder="New head count"
+            onChange={(e) => setNewHeadCount(e.target.value)}
+          />
+          <button onClick={() => updateBooking("headCount", booking)}>
+            Change Head Count
+          </button>
+          <button onClick={() => deleteBooking(booking.id)}>Delete</button>
         </div>
       ))}
     </>
