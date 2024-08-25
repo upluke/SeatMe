90ae53d1b1e0b71673c4db30f97154df78a290e1
upluke <lukeforadream@gmail.com>
2024-08-25 10:43:28 -0700

    fixed typo in models

--- server/newproject/api/models.py
+++ server/newproject/api/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Book(models.Model):
-    guess_name = models.CharField(max_length=50)
+    guest_name = models.CharField(max_length=50)
     head_count = models.IntegerField()
 
     def __str__(self):
-        return self.guess_name
+        return self.guest_name
