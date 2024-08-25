6337a668d9c83137682a65b99705e2ecaf793161
upluke <lukeforadream@gmail.com>
2024-08-25 08:02:49 -0700

    set up models

--- server/newproject/api/models.py
+++ server/newproject/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    guess_name = models.CharField(max_length=50)
+    head_count = models.IntegerField()
+
+    def __str__(self):
+        return self.guess_name
