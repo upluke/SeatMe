eb6051d1c78419ec29838b0ca91a6eac98d58610
upluke <lukeforadream@gmail.com>
2024-08-25 08:57:08 -0700

    set up create_booking endpoint

--- server/newproject/api/urls.py
+++ server/newproject/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from .views import get_bookings
+from .views import get_bookings, create_booking
 
 urlpatterns = [
-    path('bookings/', get_bookings, name = 'get_bookings')
+    path('bookings/', get_bookings, name = 'get_bookings'),
+    path('bookings/create/', create_booking, name='create_booking')
 ]
--- server/newproject/api/views.py
+++ server/newproject/api/views.py
@@ -11,4 +11,12 @@ def get_bookings(request):
     bookings = Book.objects.all()
     serializedData = BookSerializer(bookings, many=True).data
     return Response(serializedData)
-    
+
+@api_view(['POST'])
+def create_booking(request):
+    data = request.data 
+    serializer = BookSerializer(data= data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
