908d008ce02271ec4f81d542a9a2b09d0e2f3a4b
upluke <lukeforadream@gmail.com>
2024-08-25 13:41:35 -0700

    improve edit funcitonality

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -3,8 +3,11 @@ import "./App.css";
 
 function App() {
   const [bookings, setBookings] = useState([]);
-  const [guestName, setGusetName] = useState("");
-  const [headCount, setHeadCount] = useState(0);
+  const [guestName, setGuestName] = useState("");
+  const [headCount, setHeadCount] = useState(1);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentBooking, setCurrentBooking] = useState(null);
 
   const [newGuestName, setNewGuestName] = useState("");
   const [newHeadCount, setNewHeadCount] = useState(0);
@@ -45,29 +48,34 @@ function App() {
       const data = await response.json();
       // console.log(">>>", data);
       setBookings((prev) => [...prev, data]);
+      setGuestName("");
+      setHeadCount(0);
     } catch (err) {
       console.log(err);
     }
   };
 
-  const updateBooking = async (fieldToBeUpdated, booking) => {
-    const target_pk = booking.id;
-    let bookingData;
-    if (fieldToBeUpdated === "guestName") {
-      bookingData = {
-        guest_name: newGuestName,
-        head_count: booking.head_count,
-      };
-    } else {
-      bookingData = {
-        guest_name: booking.guest_name,
-        head_count: newHeadCount,
-      };
-    }
+  const openEditPopup = (booking) => {
+    setCurrentBooking(booking);
+    setNewGuestName(booking.guest_name);
+    setNewHeadCount(booking.head_count);
+    setIsEditing(true);
+  };
+
+  const closeEditPopup = () => {
+    setIsEditing(false);
+    setCurrentBooking(null);
+  };
+
+  const updateBooking = async () => {
+    const bookingData = {
+      guest_name: newGuestName,
+      head_count: newHeadCount,
+    };
 
     try {
       const response = await fetch(
-        `http://127.0.0.1:8000/api/bookings/${target_pk}`,
+        `http://127.0.0.1:8000/api/bookings/${currentBooking.id}`,
         {
           method: "PUT",
           headers: {
@@ -79,13 +87,14 @@ function App() {
       const data = await response.json();
       setBookings((prev) =>
         prev.map((booking) => {
-          if (booking.id === target_pk) {
+          if (booking.id === currentBooking.id) {
             return data;
           } else {
             return booking;
           }
         })
       );
+      closeEditPopup();
     } catch (err) {
       console.log(err);
     }
@@ -110,38 +119,49 @@ function App() {
         <input
           type="text"
           placeholder="Name"
-          onChange={(e) => setGusetName(e.target.value)}
+          value={guestName}
+          onChange={(e) => setGuestName(e.target.value)}
         />
         <input
           type="number"
           placeholder="Number of people"
-          onChange={(e) => setHeadCount(e.target.value)}
+          value={headCount}
+          onChange={(e) => setHeadCount(Number(e.target.value))}
         />
         <button onClick={addBooking}>Add Booking</button>
       </div>
+
       {bookings.map((booking) => (
         <div key={booking.id}>
           <p>Name: {booking.guest_name} </p>
           <p>Number of People: {booking.head_count}</p>
-          <input
-            type="text"
-            placeholder="New guest name"
-            onChange={(e) => setNewGuestName(e.target.value)}
-          />
-          <button onClick={() => updateBooking("guestName", booking)}>
-            Change Name
-          </button>
-          <input
-            type="number"
-            placeholder="New head count"
-            onChange={(e) => setNewHeadCount(e.target.value)}
-          />
-          <button onClick={() => updateBooking("headCount", booking)}>
-            Change Head Count
-          </button>
+
+          <button onClick={() => openEditPopup(booking)}>Edit</button>
           <button onClick={() => deleteBooking(booking.id)}>Delete</button>
         </div>
       ))}
+
+      {isEditing && (
+        <div className="popup-container">
+          <div className="popup-window">
+            <h2>Edit Booking</h2>
+            <input
+              type="tesxt"
+              value={newGuestName}
+              onChange={(e) => setNewGuestName(e.target.value)}
+              autoFocus
+            />
+            <input
+              type="number"
+              value={newHeadCount}
+              onChange={(e) => setNewHeadCount(e.target.value)}
+              autoFocus
+            />
+            <button onClick={updateBooking}>Update</button>
+            <button onClick={closeEditPopup}>Cancel</button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
